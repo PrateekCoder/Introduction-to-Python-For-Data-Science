60aaa2422ac473f24b7e7c49618dac8e3685b6b7
Prateek Gaurav <prateekgaurav23@gmail.com>
2016-07-08 21:59:26 +0530

    Commplete Python Basics for Data Science

---
+++ 01_Python_Basics/Hello_Python/script01.py
@@ -0,0 +1,24 @@
+'''
+Python as a calculator
+100xp
+Python is perfectly suited to do basic calculations. Apart from addition, subtraction, multiplication and division, there is also support for more advanced operations such as:
+
+Exponentiation: **. This operator raises the number to its left to the power of the number to its right: for example 4**2 will give 16.
+Modulo: %. It returns the remainder of the division of the number to the left by the number on its right, for example 18 % 7 equals 4.
+'''
+# Addition and subtraction
+print(5 + 5)
+print(5 - 5)
+
+# Multiplication and division
+print(3 * 5)
+print(10 / 2)
+
+# Exponentiation
+print(4 ** 2)
+
+# Modulo
+print(18 % 7)
+
+# How much is your $100 worth after 7 years?
+print(100*(1.1**7))
